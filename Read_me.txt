1b98ea65bea80a0730158c0c10d6a44f9a0b8222
MahnazHabibi <82744983+MahnazHabibi@users.noreply.github.com>
2021-06-17 19:14:58 +0430

    Add files via upload

---
+++ Read_me.txt
@@ -0,0 +1,88 @@
+A method to calculate topological features.
+
+Version 1.0
+Corresponding author: Mahnaz Habibi (Email: mhabibi@ipm.ir)
+=============
+
+
+If you want to find the topological features in new dataset, please
+organize your input#1 file in the following format:
+
+protein#1	protein#2
+---	---
+---	---
+---	---
+---	---
+---	---
+				
+Each row refers to one protein interaction pair.
+=============
+Please organize your input#2 file in the following format:
+
+protein#1
+---
+---
+---
+---
+---
+				
+Each row refers to a human protein as a drug target.
+=============
+Please organize your input#3 file in the following format:
+
+protein#1
+---
+---
+---
+---
+---
+				
+Each row refers a human protein that interact with proteins of virus.
+=============
+Please organize your input#4 file in the following format:
+
+Biological_Process,protein#1,protein#2,protein#3,
+---
+---
+---
+---
+---
+				
+Each row refers to Gene ontology terms (IBP GO term) affected by the virus and the name of proteins in Go term that separated by ','.
+for example:
+DNA protection [GO:0042262],P35520,P36639,Q9H773,Q9NS18,Q9NV35,
+==============
+
+The entry point of the code is the script Topological_Network.m
+To run the program, use the following arguments in matlab command line:
+
+"Topological_Network"
+
+
+You will see the following on the command line--
+"Please enter the name of protein interaction network file: "
+For example: 'Input#1.txt'
+
+Then you will see the following on the command line--
+"Please enter the name of all target proteins file: "
+For example: 'Input#2.txt'
+
+Then you will see the following on the command line--
+"Please enter the name of host proteins that are targeted by the virus file: "
+For example: 'Input#3.txt'
+
+Then you will see the following on the command line--
+"Please enter the name of biological processes file "
+For example: 'Input#4.txt'
+
+
+Finally, you will see the following on the command line--
+
+"Please enter the name of output file: "
+For example: 'Output.txt'
+
+The output  contain the set of all target proteins  and their topological and statisticl features in "Output.txt" file can be found .
+
+==============
+
+
---
+++ Topological_Network.m
@@ -0,0 +1,147 @@
+function []=Topological_Network()
+InteractionFile=input('Please enter the name of protein interaction network file: ');
+Co=fopen(char(InteractionFile));
+t=fgetl(Co);
+i=1;
+while t~=-1
+    x=find(t=='	');
+    Interact{i,1}=char(t(1:x-1));
+    Interact{i,2}=char(t(x+1:length(t)));
+    i=i+1;
+    t=fgetl(Co);
+end
+ListNameProtein=union(Interact(:,1),Interact(:,2));
+Adj(length(ListNameProtein),length(ListNameProtein))=0;
+for i=1:length(Interact)
+    Number1=FindNumber(Interact{i,1},ListNameProtein);
+    Number2=FindNumber(Interact{i,2},ListNameProtein); 
+    if Number1>0
+        if Number2>0
+           Adj(Number1,Number2)=1;
+           Adj(Number2,Number1)=1;
+        end
+    end
+end
+fclose(Co);
+save Adj
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+DrugTaegetFile=input('Please enter the name of all target proteins file: ');
+Co1=fopen(char(DrugTaegetFile));
+t=fgetl(Co1);
+i=1;
+while t~=-1
+    ListNameTarget{i}=char(t);
+    i=i+1;
+    t=fgetl(Co);
+end
+n=length(ListNameTarget);
+Set(1)=0;l=1;
+for i=1:n
+    Number1=FindNumber(ListNameTarget{i},ListNameProtein);
+    if Number1>0
+        Set(i)=Number1;
+        
+        LNT{l}=ListNameTarget{i};
+        l=l+1;
+    end
+end
+Set=setdiff(Set,[0]);
+fclose(Co1);
+save Set
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+HostTargetfile=input('Please enter the name of host virus targets file: ');
+Co2=fopen(char(HostTargetfile));
+t=fgetl(Co2);
+i=1;
+while t~=-1
+    ListNameHostTarget{i}=char(t);
+    i=i+1;
+    t=fgetl(Co2);
+end
+n=length(ListNameHostTarget);
+T(1)=0;
+for i=1:n
+    Number1=FindNumber(ListNameHostTarget{i},ListNameProtein);
+    if Number1>0
+        T(i)=Number1;
+    end
+end
+T=setdiff(T,[0]);
+fclose(Co2);
+save T
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+IBPFile=input('Please enter the name of biological processes file: ');
+Co3=fopen(char(IBPFile));
+t=fgetl(Co3);
+k=1;
+while t~=-1
+    x=find(t==',');
+    Process(k).GO=char(t(1:x(1)-1));
+    genes={};
+    n=length(x);
+    if n>1
+        for i=1:n-1
+            genes{i}=char(t(x(i)+1:x(i+1)-1));
+        end
+    end
+    Process(k).genes=genes;
+    genesID=[];
+    if n>1
+        for i=1:n-1
+            genes_ID(i)=FindNumber(char(genes{i}),ListNameProtein);
+        end
+    end
+    Process(k).gene_ID=setdiff(genes_ID,[0]);
+    k=k+1;
+    t=fgetl(Co);
+end
+fclose(Co3);
+save Process
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ibp=IBP(Process,Set);
+Features_Matrix=ibp';
+fprintf('load ppi data and compute the IBP(v) for each protein  ...\n');
+
+dr=DR(Adj,Set,T);
+Features_Matrix(:,2)=dr';
+fprintf('load ppi data and compute the DR(v) for each protein  ...\n');
+
+an=AN(Adj,Set);
+Features_Matrix(:,3)=an';
+fprintf('load ppi data and compute the AN(v) for each protein  ...\n');
+
+distance=Distance_Between_Vertex_H(Adj,T);
+[d]=D(distance,Set);
+Features_Matrix(:,4)=d';
+fprintf('load ppi data and compute the D(v,T) for each protein  ...\n');
+
+[md]=MD(Adj,Set,distance);
+Features_Matrix(:,5)=md';
+fprintf('load ppi data and compute the MD(v,T) for each protein  ...\n');
+
+p_ibp=P_IBP(Adj,Set,Process);
+Features_Matrix(:,6)=p_ibp';
+fprintf('load ppi data and compute the P_IBP(v) for each protein  ...\n');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+OutputFile=input('Please enter the name of output file: ');
+C=fopen(char(OutputFile),'a');
+n=length(Set);
+for i=1:n
+    fprintf(C,'%s',char(ListNameTarget{i}));
+    for j=1:6
+        fprintf(C,', %5.3f',Features_Matrix(i,j));
+    end
+    fprintf(C,'\n');
+end
+fclose(C);
+    
+
+
+
+
+
+
+
+
+
+
